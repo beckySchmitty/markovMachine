5846eafe66cd951a9b036f7c80fb7b2254362f31
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-10 11:09:30 -0500

    refactored makeText for efficency

--- makeText.js
+++ makeText.js
@@ -4,23 +4,29 @@ const fs = require("fs");
 const process = require("process");
 const axios = require("axios");
 
+// make markov machine and generate text
+function generateText(text) {
+    let mm = new markov.MarkovMachine(text);
+    console.log(mm.makeText())
+}
+
+// read file and generate text
 function markovFile (file) {
     fs.readFile(file, 'utf8', function (err, data) {
         if (err) {
             console.log(err)
             process.kill(1)
         }
-        let fileMarokov = new markov.MarkovMachine(data)
-        console.log(fileMarokov.makeText())
+        generateText(data)
     })
 }
 
+// real URL and make text
 async function marokvUrl (url) {
     try {
-        let resp = await axios.get(url)
-        console.log("RESP", resp)
-        let respMarokov = new markov.MarkovMachine(resp.data)
-        console.log(respMarokov.makeText())
+        let resp = await axios.get(url);
+        generateText(resp.data)
+
     } catch (e) {
         console.log(`Error fetching ${url}: ${e}`);
         process.exit(1);    
@@ -28,5 +34,17 @@ async function marokvUrl (url) {
 }
 
 // text from file or url into Markov output via command line call
-let words = process.argv[3];
-process.argv[2] === 'url' ? marokvUrl(words) : markovFile(words);
+let [method, path] = process.argv.slice(2);
+
+if (method === "file") {
+  markovFile(path);
+}
+
+else if (method === "url") {
+  marokvUrl(path);
+}
+
+else {
+  console.log(`Unknown method: ${method}`);
+  process.exit(1);
+}
